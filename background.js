b85d20972a882c8340ed4eed30c72b64cafef9bc
Tanner <t@b.com>
2019-04-30 20:46:14 -0500

    added no news indicator, more news button, and switched all links/more news clicks to open new tabs

--- background.js
+++ background.js
@@ -1,39 +1,50 @@
 'use strict';
 function modifyDOM() {
   let officeLevels = [].slice.call(document.getElementsByClassName("_2pio"));
-  console.log(officeLevels);
-  for(let officeLevel in officeLevels){
-    if(officeLevel %2 === 0) {
-      var officials = officeLevels[officeLevel].childNodes;
-      let officeLevelTitle = officials[0].childNodes[0].firstChild;
-      console.log(officeLevelTitle);
+  for(let officeLevelIndex in officeLevels){
+    if(officeLevelIndex %2 === 0) {
+      var officials = officeLevels[officeLevelIndex].childNodes;
       let officeMembers = [].slice.call(officials).slice(1);
-      for(let officer in officeMembers){
-        let officeMemember = officeMembers[officer].firstChild.childNodes[1].firstChild.firstChild.firstChild.innerText;
-        console.log(officeMemember);
+      for(let officerIndex in officeMembers){
+        let officeMember = officeMembers[officerIndex].firstChild.childNodes[1].firstChild.firstChild.firstChild.innerText;
+        let newsUrl = 'https://news-collector-agent.herokuapp.com/https://news.google.com/search?q=%22' +officeMember+'%22&hl=en-US&gl=US&ceid=US';
         (async () => {
-          fetch('https://news-collector-agent.herokuapp.com/https://news.google.com/search?q=%22' +officeMemember+'%22&hl=en-US&gl=US&ceid=US', {
+          fetch(newsUrl, {
             method: 'GET',
           }).then(function (response) {
             response.text().then(function(text) {
-              parser = new DOMParser();
+              const parser = new DOMParser();
               let officeMemberNewsDoc = parser.parseFromString(text, "text/html");
-              console.log(officeMemberNewsDoc);
               let officeMemberNewsDocDivs = [].slice.call(officeMemberNewsDoc.getElementsByClassName("xrnccd")).slice(0,5);
-              for(let officeMemberNewsDocDiv in officeMemberNewsDocDivs){
-                console.log(officeMemberNewsDocDivs[officeMemberNewsDocDiv].getElementsByTagName("a"));
-                let links = [].slice.call(officeMemberNewsDocDivs[officeMemberNewsDocDiv].getElementsByTagName("a"));
-                for(let link in links){
-                  links[link].href = links[link].href.replace("./","https://news.google.com");
+              if(officeMemberNewsDocDivs.length === 0){
+                let noNewsDiv = document.createElement("div");
+                noNewsDiv.className = "_2pio";
+                noNewsDiv.style.textAlign = "center";
+                noNewsDiv.innerText = "No News Found";
+                officeMembers[officerIndex].append(noNewsDiv);
+              }else {
+                for (let officeMemberNewsDocDivIndex in officeMemberNewsDocDivs) {
+                  let links = [].slice.call(officeMemberNewsDocDivs[officeMemberNewsDocDivIndex].getElementsByTagName("a"));
+                  for (let link in links) {
+                    links[link].href = links[link].href.replace("./", "https://news.google.com");
+                    links[link].target = "_blank";
+                  }
+                  officeMemberNewsDocDivs[officeMemberNewsDocDivIndex].className = "_2pio";
+                  let elements = officeMemberNewsDocDivs[officeMemberNewsDocDivIndex].getElementsByTagName('div');
+                  while (elements[0]) elements[0].parentNode.removeChild(elements[0]);
+                  elements = officeMemberNewsDocDivs[officeMemberNewsDocDivIndex].getElementsByTagName('figure');
+                  while (elements[0]) elements[0].parentNode.removeChild(elements[0]);
+                  officeMembers[officerIndex].append(officeMemberNewsDocDivs[officeMemberNewsDocDivIndex]);
                 }
-                officeMemberNewsDocDivs[officeMemberNewsDocDiv].className = "_2pio";
-                let elements = officeMemberNewsDocDivs[officeMemberNewsDocDiv].getElementsByTagName('div');
-                while (elements[0]) elements[0].parentNode.removeChild(elements[0]);
-                elements = officeMemberNewsDocDivs[officeMemberNewsDocDiv].getElementsByTagName('figure');
-                while (elements[0]) elements[0].parentNode.removeChild(elements[0]);
-                officeMembers[officer].append(officeMemberNewsDocDivs[officeMemberNewsDocDiv]);
+                let moreNewsDiv = document.createElement("div");
+                let moreNewsButton = document.createElement("button");
+                moreNewsButton.onclick = function(){window.open(newsUrl, '_blank');};
+                moreNewsButton.textContent = "More News";
+                moreNewsDiv.append(moreNewsButton);
+                moreNewsDiv.style.textAlign = "center";
+                moreNewsDiv.className = "_2pio";
+                officeMembers[officerIndex].append(moreNewsDiv);
               }
-              console.log(officeMemberNewsDocDivs);
             });
           });
         })();
